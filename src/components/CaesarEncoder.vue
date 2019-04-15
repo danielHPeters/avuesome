217a2290eaedea47585f27aa6151a6871f01ba07
Daniel Peters <daniel.peters.ch@gmail.com>
2019-04-15 15:58:35 +0200

    Add copy to clipboard functionality.

--- src/components/CaesarEncoder.vue
+++ src/components/CaesarEncoder.vue
@@ -2,34 +2,48 @@
   <div class="card">
     <div class="card-content">
       <div class="input-field">
-        <input id="keyInput" type="number" v-model="currentKey" step="1" min="1" max="25"/>
+        <input id="keyInput" type="number" v-model="currentKey" step="1" min="1" max="25" @input="updateCaesarCode"/>
         <label for="keyInput">Key</label>
       </div>
       <div class="input-field">
-        <input id="userInput" type="text" v-model="text"/>
+        <input id="userInput" type="text" v-model="text" @input="updateCaesarCode"/>
         <label for="userInput">Enter a text</label>
       </div>
-      <p>Encrypted message: {{ text.length > 0 ? encrypt(text, currentKey) : ''}}</p>
+      <div class="input-field">
+        <label for="caesarCode">Encrypted message</label>
+        <textarea id="caesarCode" class="materialize-textarea" readonly ref="caesarCode" v-model="caesarCode"></textarea>
+      </div>
+      <div class="btn waves-effect waves-light" @click="copyToClipBoard()">
+        <i class="material-icons left">content_copy</i>Copy to clipboard
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import * as M from 'materialize-css'
+
 export default {
   name: 'CaesarEncoder',
   data: () => {
     return {
       text: '',
+      caesarCode: '',
       currentKey: 3
     }
   },
+  mounted: () => {
+    M.updateTextFields()
+  },
+  updated: () => {
+    M.updateTextFields()
+  },
   methods: {
+    updateCaesarCode () {
+      this.caesarCode = this.encrypt(this.text, this.currentKey)
+    },
     modulo (n, p) {
-      if (n < 0) {
-        n = p - Math.abs(n) % p
-      }
-
-      return n % p
+      return ((n % p) + p) % p
     },
     encrypt (message, key) {
       const ranges = [[65, 90], [97, 122]] // A-Z & a-z
@@ -40,8 +54,11 @@ export default {
         let foundCode = false
 
         for (let j = 0; j < ranges.length; j++) {
-          if (code >= ranges[j][0] && code <= ranges[j][1] && !foundCode) {
-            code = this.modulo((code - ranges[j][0] + Number(key)), (ranges[j][1] - ranges[j][0] + 1)) + ranges[j][0]
+          const base = ranges[j][0]
+          const max = ranges[j][1]
+
+          if (code >= base && code <= max && !foundCode) {
+            code = this.modulo((code - base + Number(key)), (max - base + 1)) + base
             foundCode = true
           }
         }
@@ -50,10 +67,33 @@ export default {
       }
 
       return encryptedMessage
+    },
+    copyToClipBoard () {
+      const code = this.$refs['caesarCode']
+      const errorMessage = 'Oops, unable to copy'
+
+      code.focus()
+      code.select()
+
+      try {
+        const successful = document.execCommand('copy')
+        const message = successful ? 'Code successfully copied to clipboard' : errorMessage
+
+        M.toast({ html: message })
+      } catch (err) {
+        M.toast({ html: errorMessage })
+      }
+
+      /* unselect the range */
+      code.setAttribute('type', 'hidden')
+      window.getSelection().removeAllRanges()
     }
   }
 }
 </script>
 
 <style scoped>
+  textarea {
+    resize: none;
+  }
 </style>
