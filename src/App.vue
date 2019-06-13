37dff20931acadbfd5c39e87e0fed9205e51201f
Daniel Peters <daniel.peters.ch@gmail.com>
2019-06-13 16:03:03 +0200

    Convert to typescript class component.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Create your secret messages!"/>
+    <CaesarEncoder/>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import CaesarEncoder from './components/CaesarEncoder.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  @Component({
+    components: {
+      CaesarEncoder
+    }
+  })
+
+export default class App extends Vue {}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
--- src/components/CaesarEncoder.vue
+++ src/components/CaesarEncoder.vue
@@ -2,7 +2,8 @@
   <div class="card">
     <div class="card-content">
       <div class="input-field">
-        <input id="keyInput" type="number" v-model.number="currentKey" step="1" min="1" max="25" @input="updateCaesarCode"/>
+        <input id="keyInput" type="number" v-model.number="currentKey" step="1" min="1" max="25"
+               @input="updateCaesarCode"/>
         <label for="keyInput">Key</label>
       </div>
       <div class="input-field">
@@ -11,7 +12,8 @@
       </div>
       <div class="input-field">
         <label for="caesarCode">Encrypted message</label>
-        <textarea id="caesarCode" class="materialize-textarea" readonly ref="caesarCode" v-model="caesarCode"></textarea>
+        <textarea id="caesarCode" class="materialize-textarea" readonly ref="caesarCode"
+                  v-model="caesarCode"></textarea>
       </div>
       <div class="btn waves-effect waves-light" @click="copyToClipBoard()">
         <i class="material-icons left">content_copy</i>Copy to clipboard
@@ -20,57 +22,61 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import * as M from 'materialize-css'
+import { Component, Prop, Vue } from 'vue-property-decorator'
 
-export default {
-  name: 'CaesarEncoder',
-  data: () => {
-    return {
-      text: '',
-      caesarCode: '',
-      currentKey: 3
-    }
-  },
-  mounted: () => {
+@Component
+export default class CaesarEncoder extends Vue {
+  private text = ''
+  private caesarCode = ''
+  private currentKey = 3
+
+  mounted (): void {
     M.updateTextFields()
-  },
-  updated: () => {
+  }
+
+  updated (): void {
     M.updateTextFields()
-  },
-  methods: {
-    updateCaesarCode () {
-      this.caesarCode = this.encrypt(this.text, this.currentKey)
-    },
-    modulo (n, p) {
-      return ((n % p) + p) % p
-    },
-    encrypt (message, key) {
-      const ranges = [[65, 90], [97, 122]] // A-Z & a-z
-      let encryptedMessage = ''
+  }
 
-      for (let i = 0; i < message.length; i++) {
-        let code = message.charCodeAt(i)
-        let isCodeFound = false
+  updateCaesarCode (): void {
+    this.caesarCode = this.encrypt(this.text, this.currentKey)
+  }
 
-        for (let j = 0; j < ranges.length; j++) {
-          const base = ranges[j][0]
-          const max = ranges[j][1]
+  modulo (n: number, p: number): number {
+    return ((n % p) + p) % p
+  }
 
-          if (code >= base && code <= max && !isCodeFound) {
-            code = this.modulo((code - base + key), (max - base + 1)) + base
-            isCodeFound = true
-          }
+  encrypt (message: string, key: number): string {
+    const ranges = [[65, 90], [97, 122]] // A-Z & a-z
+    let encryptedMessage = ''
+
+    for (let i = 0; i < message.length; i++) {
+      let code = message.charCodeAt(i)
+      let isCodeFound = false
+
+      for (let j = 0; j < ranges.length; j++) {
+        const base = ranges[j][0]
+        const max = ranges[j][1]
+
+        if (code >= base && code <= max && !isCodeFound) {
+          code = this.modulo((code - base + key), (max - base + 1)) + base
+          isCodeFound = true
         }
-
-        encryptedMessage += String.fromCharCode(code)
       }
 
-      return encryptedMessage
-    },
-    copyToClipBoard () {
-      const codeElement = this.$refs['caesarCode']
-      const selection = getSelection()
+      encryptedMessage += String.fromCharCode(code)
+    }
+
+    return encryptedMessage
+  }
+
+  copyToClipBoard (): void {
+    const codeElement = this.$refs['caesarCode'] as HTMLFormElement
+    const selection = getSelection()
+
+    if (selection) {
       const previousSelection = selection.rangeCount > 0 ? selection.getRangeAt(0) : false
       const errorMessage = 'Oops, unable to copy!'
       const successMessage = 'Code successfully copied to clipboard'
